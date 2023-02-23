0f44d4bd17f300bd7a2ae841fa2166f0117f0c15
Pathuri Sai Saran <99058160+SaiSaranPathuriNEU@users.noreply.github.com>
2023-02-23 16:14:48 -0500

    terraform commit

--- terraform/module/networking/variables.tf
+++ terraform/module/networking/variables.tf
@@ -24,3 +24,18 @@ variable "aws_availability_zones" {
   #default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
+variable "ami" {
+  type    = string
+  default = "ami-0acfa6d36b89d5dcb"
+}
+
+variable "key_name" {
+  type    = string
+  default = "ec2_dev"
+}
+
+# variable "ssh_key" {
+#   type    = string
+#   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFoJMCuNdwdCE4WNpBjWyljoQ9+Qr+JxfnHnZ/emuKZQTRSWHfKa+sEmZy9OG1xGoEVbzjb00UogzOQTnlFxd/s6odlQzaBRlCbpPKhrG7q6pdWrXosu21HDMbqgW4Tg73v+rTmiSis3Yh5ZN+s/xu/1lNQ5OYdSM/PFx7lcZ3HkVd9SX24HX322MsZPuvvPNs4/xAD30Z7OK+ymAxQ+ZZKWL9Sv4TDthVatFoNYEgnF4wWSuHvf2ZmE8EId36/fs/DumrCcJJxq2yT/FatkDF2XQeA/gR1MOYGvOjGMRnDdez3B4xtUoJmZD6AWquHcNbIYN/YGA8whymqN/WwfnQWepY0zURoxb9HRTl70ddNho72Gurzw13Ph9FLxY+PwnZnxxo/AO/E8j56Ddhl2RnzH8H0xGSZcFG59hWMzjAuq7R1ljd3zEfqFTrWRE5F1iwKLHddhGPcaxn1zupkLvIINnLQRnDp1Px5ONJpY4n0/e5nqIR+rYTOn7fWEZeLp8= saisaranpathuri@Sais-MacBook-Air-2.local"
+# }
+
